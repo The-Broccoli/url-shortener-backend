4b0708c9a70d09322d3d26f7e7c8f97a4deb49be
The-Broccoli <The-Broccoli@users.noreply.github.com>
2024-03-06 12:10:34 +0100

    Update settings.py

--- url_shortener_backend/settings.py
+++ url_shortener_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t@c6dwek$lq7_98pb@a=he*9_%u3i=!^+!v835&+w*5^y@gjoo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200/'
